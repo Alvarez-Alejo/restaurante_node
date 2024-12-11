6aa39e06777284896de61afbd1b520d0fa239ef6
Alejo <alejoalvarez876@gmail.com>
2024-12-11 20:02:57 -0300

    tengo un error con la base de datos

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -2,39 +2,41 @@ var express = require('express');
 var router = express.Router();
 var usuariosModel = require('./../../models/usuariosModel');
 
-router.get('/', function(req, res, next) {
-    res.render('admin/login', {
+router.get('/', function (req, res, next) {
+  res.render('admin/login', {
     layout: 'admin/layout'
-    });
   });
+});
 
-  router.get('/logout', function(req, res, next) {
-    req.session.destroy();
-    res.render('admin/login', {
-      layout: 'admin/layout'
-    });
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: 'admin/layout'
   });
-  router.post('/', async(req, res, next)=>{
-    try{
-      console.log(req.body);
+});
+
+router.post('/', async (req, res, next) => {
+  try {
 
     var usuario = req.body.usuario;
     var password = req.body.password;
-    
-    var data=await usuariosModel.getUserAndPassword(usuario, password);
-    if(data != undefined){
+    console.log(usuario,password)
+    var data = await usuariosModel.getUserAndPassword(usuario, password);
+    console.log(data);
+    if (data != undefined) {
       req.session.id_usuario = data.id;
       req.session.nombre = data.usuario;
-     
+
       res.redirect('/admin/novedades');
-     } else{
+    } else {
       res.render('admin/login', {
         layout: 'admin/layout',
-        error: true    
-       })
+        error: true
+      })
     } //cierre else
-    } catch(error){
-      console.log(error)
-    }
-  });
-  module.exports = router;
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+module.exports = router;

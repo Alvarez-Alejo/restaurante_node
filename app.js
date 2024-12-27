acd449ed32ec6784c49e2f9cf8687f84066236ee
Alejo <alejoalvarez876@gmail.com>
2024-12-27 15:05:26 -0300

    PROYECTO FINAL ALEJO ALVAREZ

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
-
 var session= require('express-session');
+var fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,6 +43,11 @@ secured= async(req,res,next)=>{
     console.log(error);
   }
 }
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
